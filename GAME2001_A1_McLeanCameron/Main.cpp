a698e1fb771c79bc6632a732ab5a556b87988e36
Cameron <mcleancam22@gmail.com>
2021-10-10 18:44:14 -0400

    Bug?

--- GAME2001_A1_McLeanCameron/Main.cpp
+++ GAME2001_A1_McLeanCameron/Main.cpp
@@ -1,10 +1,36 @@
-#include "OrderedArray.h"
+#include "UnorderedArray.h"
 #include "OrderedArray.h"
 #include <iostream>
 using namespace std;
 
 int main()
 {
+	cout << "Merge Sort Algorithm" << endl;
+
+	UnorderedArray<int> array(5);
+	array.push(80);
+	array.push(64);
+	array.push(99);
+	array.push(76);
+	array.push(5);
+
+	cout << "Before Sorting: ";
+	for (int i = 0; i < array.GetSize(); i++)
+	{
+		cout << " " << array[i];
+	}
+	cout << endl;
+
+	// Call our sort
+	array.MergeSort();
+
+	cout << "After sorting: ";
+	for (int i = 0; i < array.GetSize(); i++)
+	{
+		cout << " " << array[i];
+	}
+
+	cout << endl << endl;
 
 	return 0;
 }
